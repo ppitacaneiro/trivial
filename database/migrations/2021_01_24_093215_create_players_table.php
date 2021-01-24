e4ae1011096fc7182824b8c8ff9eeba57179de23
pablo <ppitacaneiro@gmail.com>
2021-01-24 10:44:51 +0100

    Inicial commit

---
+++ database/migrations/2021_01_24_093215_create_players_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('nickname',60);
+            $table->string('email',255);
+            $table->integer('age');
+            $table->enum('sex',['male','female']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
